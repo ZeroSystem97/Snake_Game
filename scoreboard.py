e4d07d29d14bce4522f172a6ac5a9239c1f26a78
Róbert Palša <ZeroSystem97@users.noreply.github.com>
2025-04-02 19:30:29 +0200

    Add Highscore
    Added Highscore, reading and writing to file.

--- scoreboard.py
+++ scoreboard.py
@@ -8,14 +8,26 @@ class Scoreboard(Turtle):
         self.hideturtle()
         self.goto(0, 280)
         self.score = 0
-        self.write(f"Score: {self.score}", align="center", font=("Arial", 16, "bold"))
+        with open("data.txt", "r") as file:
+            self.highscore = int(file.read())
+        self.update_scoreboard()
 
+    def update_highscore(self):
+        with open("data.txt", "w") as file:
+            file.write(str(self.highscore))
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align="center", font=("Arial", 24, "bold"))
-
-    def update(self):
+    def update_scoreboard(self):
         self.clear()
+        self.write(f"Score: {self.score} High Score: {self.highscore}", align="center", font=("Arial", 16, "bold"))
+
+
+    def reset(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+            self.update_highscore()
+        self.score = 0
+        self.update_scoreboard()
+
+    def increase_score(self):
         self.score += 1
-        self.write(f"Score: {self.score}", align="center", font=("Arial", 16, "bold"))
+        self.update_scoreboard()
